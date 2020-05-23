bada95843e9ae6deebf89f0bdcb9d95e6c6239db
Dedan Msafari <dedan.msafari@gmail.com>
2020-05-23 08:15:57 +0300

    Added containers and routing elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { CssBaseline } from '@material-ui/core';
+import { ToastContainer, toast } from "react-toastify";
 import { Switch, Route , Redirect} from 'react-router-dom'
 import Home from './components/home';
 import Resume from './components/resume';
+import Notfound from './components/notFound';
 import Contacts from './components/contacts';
 import Portfolio from './components/portfolio';
 import './App.css';
@@ -12,11 +14,19 @@ import './App.css';
 function App() {
   return (
     <>
+    
   <CssBaseline/>
-    <Route exact path="/" component={Home}/>
+     <ToastContainer />
+     <Switch>
+
+    <Route exact path="/home" component={Home}/>
     <Route  path="/portfolio" component={Portfolio}/>
     <Route path="/resume" component={Resume}/>
     <Route path="/contacts" component={Contacts}/>
+    <Route path="/not-found" component={Notfound} />
+    <Redirect from="/" exact to="/home" />
+    <Redirect to="/not-found" />
+     </Switch>
 </>
   );
 }
